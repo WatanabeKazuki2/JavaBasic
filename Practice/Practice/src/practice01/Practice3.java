89b981a169dd4d7df8c93a5c8cca1617a385c61a
DESKTOP-0G656D9\LIKEIT_STUDENT <tomoki.watanabe.likeit@gmail.com>
2018-11-01 13:12:08 +0900

    practice 3

--- Practice/Practice/src/practice01/Practice3.java
+++ Practice/Practice/src/practice01/Practice3.java
@@ -2,8 +2,8 @@ package practice01;
 
 public class Practice3 {
 	public static void main(String[] args) {
-		double  Pl = 3.14;
-			System.out.println(Pl);
+	double  Pl = 3.14;
+	System.out.println(Pl);
 	}
 
 }
--- Practice/Practice/src/practice01/practice1.java
+++ Practice/Practice/src/practice01/practice1.java
@@ -4,7 +4,7 @@ public class practice1 {
 	public static void main(String[] args) {
 	//演習１
 		int year;
-			year = 2017;
-			System.out.println(year);
+		year = 2017;
+		System.out.println(year);
 	}
 }
